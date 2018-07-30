ab2966c6fd689690a36a573c0c0a0bfff1032f25
Ruben <ruben.carrizales.r@hotmail.com>
2018-07-30 16:55:43 -0500

    initial commit

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -2,14 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threads
 {
     class Program
     {
+
+        public static void CallToChildThread()
+        {
+            try
+            {
+                Console.WriteLine("Child thread starts");
+
+                // do some work, like counting to 10
+                for (int counter = 0; counter <= 10; counter++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine(counter);
+                }
+
+                Console.WriteLine("Child Thread Completed");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("Thread Abort Exception: {0}", e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Couldn't catch the Thread Exception");
+            }
+        }
         static void Main(string[] args)
         {
+            ThreadStart childref = new ThreadStart(CallToChildThread);
+            Console.WriteLine("In Main: Creating the Child thread");
+
+            Thread childThread = new Thread(childref);
+
+            Console.WriteLine(childThread.ThreadState);
+
+            childThread.Start();
+
+            Console.WriteLine(childThread.ThreadState);
+
+            //stop the main thread for some time
+            Thread.Sleep(2000);
+
+            //now abort the child
+            Console.WriteLine("In Main: Aborting the Child thread");
+
+            childThread.Abort();
+            Console.ReadKey();
         }
+
+
     }
 }
